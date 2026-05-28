d1adac486644b49de6d95e611a81a25de6996173
Juliveson <juliveson26@gmail.com>
2026-05-29 02:51:35 +0300

    update admin url

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('turn-admin/', admin.site.urls),
     path("", include("core.urls")),
 ]
 
